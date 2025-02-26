9c16eb90bc081ae0a0ddc8c98c7183cb80cf82cc
adnan.shaikh_ftc <adnan.shaikh_ftc@external.swiggy.in>
2025-02-26 23:42:38 +0530

    Craeted SecurityConfig to add an admin and also connected it with postgres

---
+++ src/main/java/org/example/categoryservice/Config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package org.example.categoryservice.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/restaurants/**", "/menu-items/**").hasRole("ADMIN") // Restrict catalog APIs to Admin
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults()); // Enable Basic Auth
+
+        return http.build();
+    }
+}
